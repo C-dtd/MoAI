2318cc7c036af27ecf6e2c3fd076c6ce2b12cb03
C-dtd <cdtd0516@gmail.com>
2024-08-22 09:33:19 +0900

    123

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const app = require('express')();
+const express =  require('express');
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const { Pool } = require('pg');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const db = new Pool({
     user: 'postgres.vpcdvbdktvvzrvjfyyzm',
     host: 'aws-0-ap-southeast-1.pooler.supabase.com',
@@ -12,6 +15,18 @@ const db = new Pool({
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000
 });
+
+app.use(cookieParser());
+app.use(session({
+    resave: true,
+    // saveUninstallized: false,
+    secret: 'secret'
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+
 
 const port = 8000;
 
@@ -25,11 +40,26 @@ io.on('connection', (socket) => {
     })
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname +'/chat.html');
+app.get('/chat', function(req, res) {
+    res.sendFile(__dirname +'/html/chat.html');
 });
 
 app.get('/db', async function(req, res) {
     const data = await db.query('select * from products');
     res.send(data.rows);
 });
+
+app.get('/', function(req, res) {
+    // const { user } = req.session;
+    // if (user) {
+    //     res.render('login', { user });
+    //     return;
+    // }
+    res.render('index.ejs');
+});
+app.post('/', function(req, res) {
+    const { name } = req.body;
+    req.session.user = name;
+    console.log(req.body);
+    res.redirect('/');
+})
