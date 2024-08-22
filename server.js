0341124a690b6e34de859b6c7d1d52e62be2241c
정성문 <palebluedot901@gmail.com>
2024-08-22 10:37:00 +0900

    seongmoon commit

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const { Pool } = require('pg');
+
+// Database configuration
 const db = new Pool({
     user: 'postgres.vpcdvbdktvvzrvjfyyzm',
     host: 'aws-0-ap-southeast-1.pooler.supabase.com',
@@ -15,21 +18,33 @@ const db = new Pool({
 
 const port = 8000;
 
+// Serve static files from 'css' and 'html' directories
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/html', express.static(__dirname + '/html'));
+
+// Route to serve the login page
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + '/html/login.html'); // Serve login.html
+});
+
+// Route to serve other static files or API endpoints
+app.get('/db', async function(req, res) {
+    const data = await db.query('select * from products');
+    res.send(data.rows);
+});
+
 server.listen(port, function() {
-    console.log('server host in http://localhost:' +port);
+    console.log('Server host in http://localhost:' + port);
 });
 
 io.on('connection', (socket) => {
     socket.on('msg', (msg) => {
         io.emit('msg', msg);
-    })
+    });
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname +'/chat.html');
-});
 
-app.get('/db', async function(req, res) {
-    const data = await db.query('select * from products');
-    res.send(data.rows);
-});
+// 지금 해야될 거
+// 1. 로그인 페이지 만들기 X
+// 2. 회원가입 페이지 만들고 연결
+// 3. 회원가입 정보 데이터베이스 로드
