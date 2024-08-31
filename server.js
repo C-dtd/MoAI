ba4b6d6d410882ae384bc8b2862c97418a5ab1b5
C-dtd <cdtd0516@gmail.com>
2024-08-31 16:17:42 +0900

    병합 충돌 해결

--- server.js
+++ server.js
@@ -126,6 +126,10 @@ app.get('/createreport', function(req, res) {
     res.render('createreport.ejs');
 });
 
+app.get('/createreport', function(req, res) {
+    res.render('createreport.ejs');
+});
+
 app.get('/register_confirm', function(req, res){
     const { name } = req.session;
     if (name) {
